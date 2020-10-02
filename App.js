548d34b9ad0eaabda180808a580b6ea6bebeb3cd
krispy <nwoaoa6@gmail.com>
2020-10-01 22:14:24 -0600

    testing api login

--- App.js
+++ App.js
@@ -3,6 +3,15 @@
  * @flow strict-local
  */
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome5';
+
+FontAwesomeIcon.getStyledIconSet('brand').loadFont();
+FontAwesomeIcon.getStyledIconSet('light').loadFont();
+FontAwesomeIcon.getStyledIconSet('regular').loadFont();
+FontAwesomeIcon.getStyledIconSet('solid').loadFont();
+Icon.loadFont();
+
 import React, { Component } from 'react';
 
 import { Provider } from 'react-redux';
--- app/modules/auth/constants.js
+++ app/modules/auth/constants.js
@@ -5,11 +5,11 @@ export const LOGGED_IN = 'auth/LOGGED_IN';
 export const LOGGED_OUT = 'auth/LOGGED_OUT';
 
 //API URL
-export const API_URL = 'https://reqres.in/api';
+export const API_URL = 'http://192.168.1.48:3000/api';
 
 //API End Points
 export const REGISTER = `${API_URL}/register`;
-export const LOGIN = `${API_URL}/login`;
+export const LOGIN = `${API_URL}/v1/auth/sign_in`;
 export const LOGOUT = `${API_URL}/logout`;
 export const FORGOT_PASSWORD = `${API_URL}/forgot`;
 
